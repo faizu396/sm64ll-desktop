fde516c609bc7bfccc92dc5b5f9eb0dd2f9805ac
faizu <102569285+faizu396@users.noreply.github.com>
2022-05-06 20:47:26 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@
 
 sudo apt-get install -y python3 python3-pip python3-tk libsdl2-dev git gcc-mips-linux-gnu
 pip3 install pysimplegui
+sudo mv ~/Downloads/sm64ll-pi/sm64launcher.desktop ~/.local/share/applications
